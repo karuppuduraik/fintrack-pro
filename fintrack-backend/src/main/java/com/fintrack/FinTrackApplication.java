23baf7b010712a20f90331a409aafe5e710788dc
karuppuduraik <karuppuduraikece@gmail.com>
2026-07-03 17:48:58 +0530

    Fix Spring Boot executable jar

--- fintrack-backend/src/main/java/com/fintrack/FinTrackApplication.java
+++ fintrack-backend/src/main/java/com/fintrack/FinTrackApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinTrackApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FinTrackApplication.class, args);
     }
